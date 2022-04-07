dc6fd2915aa729226e3adac458a775aac7beed06
Jack Lee <jkprogramador@gmail.com>
2022-04-07 08:21:22 -0300

    Add TailwindCSS.

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -13,6 +13,11 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "vuejs-accessibility/label-has-for": ["error", {
+      "required": {
+        "some": ["nesting", "id"]
+      }
+    }],
   },
   overrides: [
     {
