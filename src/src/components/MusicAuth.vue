81eb78b69bd3022f2d3eb2c7090145f23f70fd31
Jack Lee <jkprogramador@gmail.com>
2022-04-12 09:14:35 -0300

    Validating Numbers

--- src/src/components/MusicAuth.vue
+++ src/src/components/MusicAuth.vue
@@ -213,7 +213,8 @@
                             <label for="register-age" class="inline-block mb-2"
                                 >Age</label
                             >
-                            <input
+                            <vee-field
+                                name="age"
                                 id="register-age"
                                 type="number"
                                 class="
@@ -229,6 +230,7 @@
                                     rounded
                                 "
                             />
+                            <ErrorMessage class="text-red-600" name="age" />
                         </div>
                         <!-- Password -->
                         <div class="mb-3">
@@ -365,7 +367,7 @@ export default {
             schema: {
                 name: "required|min:3|max:100|alpha_spaces",
                 email: "required|min:3|max:100|email",
-                age: "",
+                age: "required|min_value:18|max_value:100",
                 password: "",
                 confirm_password: "",
                 country: "",
--- src/src/includes/validation.js
+++ src/src/includes/validation.js
@@ -1,5 +1,5 @@
 import { Form as VeeForm, Field as VeeField, defineRule, ErrorMessage } from "vee-validate";
-import { required, min, max, alpha_spaces as alphaSpaces, email } from "@vee-validate/rules";
+import { required, min, max, alpha_spaces as alphaSpaces, email, min_value as minValue, max_value as maxValue } from "@vee-validate/rules";
 
 export default {
     install(app) {
@@ -12,5 +12,7 @@ export default {
         defineRule('min', min);
         defineRule('alpha_spaces', alphaSpaces);
         defineRule('email', email);
+        defineRule('min_value', minValue);
+        defineRule('max_value', maxValue);
     },
 }
